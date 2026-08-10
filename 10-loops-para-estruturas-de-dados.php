0b04d68eb7309d5cca833cfe6c3680874a913edc
adelaeesquivel-prog <adelaeesquivel@gmail.com>
2026-08-10 10:40:54 -0300

    usando foreach

--- 10-loops-para-estruturas-de-dados.php
+++ 10-loops-para-estruturas-de-dados.php
@@ -40,6 +40,18 @@ for($i = 0; $i < $linhas; $i++): //acessa cada linha
     endfor; //fim do acesso de cada linha
  ?>
 
+ <hr>
+
+ <h2>Usando loop foreach para Array</h2>
+ <?php 
+ $alunos =["Thiago", "Adela","Renan","Pérola"];
+ foreach($alunos as $aluno):
+ ?>
+    <p><?= $aluno ?></p>
+<?php 
+endforeach;
+?>
+
 
     </div>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">   
