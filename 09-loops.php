3c5ad409c734e2be4ee8654e249b217aff4a3d27
adelaeesquivel-prog <adelaeesquivel@gmail.com>
2026-08-10 09:40:05 -0300

    Update 09-loops.php

--- 09-loops.php
+++ 09-loops.php
@@ -7,6 +7,7 @@
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
+    <div class="container">
     <h1>Trabalhando com comando de repetição</h1>
     <hr>
 
@@ -52,6 +53,7 @@ do {
         <p>Resposta<?= $i ?></p>
     </details>
 <?php }  //ou endfor; ?>
+</div>
 
 </section>  
 
