fe907f28c7e0a43b3607611908c75eecd3bea3f7
adelaeesquivel-prog <adelaeesquivel@gmail.com>
2026-08-10 10:35:41 -0300

    loops for tradicional

---
+++ 10-loops-para-estruturas-de-dados.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html lang="pt-bre">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+ </head>
+<body>
+    <div class="container">
+        <h1>Loops para estruturas de dados</h1>
+        <hr>
+<?php 
+$meses =["Janeiro", "Fevereiro","Março", "Abril", "Maio","Junho"];
+?>
+     <h2>Usando o loop for para acessar array</h2>
+     <ol>
+ <?php for ($i = 0; $i < count ($meses); $i++){  ?>
+        <li><?= $meses[$i] ?></li>
+<?php } ?>
+      </ol>
+
+      <hr>
+      <h2>Usando o loop for para acessar uma matriz (array e arrays)</h2>
+<?php
+$planoDeEstudos = [
+    ["JS Avançado","Node.js","Next.js"],
+    ["PHP","Orientação a Objetos"],
+    ["Teoria das Cores", "Photoshop com IA","UX/UI"]
+];
+$linhas = count ($planoDeEstudos);
+for($i = 0; $i < $linhas; $i++): //acessa cada linha
+    $colunas = count($planoDeEstudos [$i]);
+    for($j = 0; $j < $colunas; $j++): //acessa cada coluna
+?>
+    <p><?= $planoDeEstudos [$i][$j] ?></p>
+ <?php 
+     endfor; //fim do acessode cada coluna
+    endfor; //fim do acesso de cada linha
+ ?>
+
+
+    </div>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">   
+</body>
+</html>
--- README.md
+++ README.md
@@ -37,7 +37,7 @@ Importar para pagina HTML (mesmo q seja PHP) as referencias dos arquivos **CSS**
 
  ## JS
  ```html
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   ```
 
 Para mais informaçoes , é necessario usar a documentação  do Bootstrap : https://getbootstrap.com/docs/5.3/getting-started/introduction/
