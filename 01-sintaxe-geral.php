35209a82f97033df8df66fd0e89bad1b8062ccce
adelaeesquivel-prog <adelaeesquivel@gmail.com>
2026-08-10 09:24:26 -0300

    aplicandi linhas bootstrap nos exemplos

--- 01-sintaxe-geral.php
+++ 01-sintaxe-geral.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP- sintaxe- geral</title>
+
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
    <!-- CSS Interno  -->
    <style>
         .exemplo{
@@ -41,6 +44,9 @@
       echo "<p><abbr title=\"HyperText Preprocessor\">PHP</abbr>
       é uma linguagem popular</p>";
     ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    
     <script>
         const tituloH1 = document.querySelector('h1');
         tituloH1.addEventListener("click", function(){
--- exercicios/exercicio-02.php
+++ exercicios/exercicio-02.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercicio 02</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <style>
         .container{
             display: flex;
@@ -62,6 +63,8 @@ $pessoa2->cidade = "Rio de Janeiro";
         <p>Idade: <?= $pessoa2->idade?> anos</p>
     </article>
    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
  
