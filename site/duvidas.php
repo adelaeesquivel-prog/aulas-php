10fd4764373432ddb2c168af1f7e4273094879e3
adelaeesquivel-prog <adelaeesquivel@gmail.com>
2026-08-19 12:00:06 -0300

    planos

---
+++ site/duvidas.php
@@ -0,0 +1,13 @@
+<?php require "includes/cabecalho.php" ?>
+
+        <h2>Página de duvidas</h2>
+        <p>Perguntas e respostas frequentes:</p>
+        <details>
+                <summary>Quem sera o proximo reprovado?</summary>
+                <p>Perola!!!</p>
+        </details>
+        <details>
+                <summary>Quem vai pagar umas paçocas?</summary>
+                <p>Anderson</p>
+        </details>
+<?php require "includes/rodape.php" ?>
---
+++ site/planos.php
@@ -0,0 +1,5 @@
+<?php require "includes/cabecalho.php" ?>
+
+       <h2>Pagina de planos</h2>
+       <p>Veja nossos planos</p>
+<?php require "includes/rodape.php" ?>
