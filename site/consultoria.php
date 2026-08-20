91128c1b55d986ddfcc088e82ea1618895b65a49
adelaeesquivel-prog <adelaeesquivel@gmail.com>
2026-08-20 08:39:34 -0300

    site

---
+++ site/consultoria.php
@@ -0,0 +1,6 @@
+<?php require "includes/cabecalho.php" ?>
+
+        <h2>Pagina consultoria</h2>
+        <p>Nossos profissionais estao a disposição</p>
+        
+<?php require "includes/rodape.php" ?>
--- site/includes/cabecalho.php
+++ site/includes/cabecalho.php
@@ -13,10 +13,11 @@ const BASE = "/site/";
     <header>
         <h1>Site com PHP</h1>
         <nav>
-            <a href="<?= BASE ?>index.php">Home</a>
-            <a href="<?= BASE ?>cursos.php">Cursos</a>
-            <a href="<?= BASE ?>duvidas.php">Dúvidas</a>
-            <a href="<?=BASE?>planos.php">Planos</a>
+            <a href="<?= BASE?>index.php">Home</a>
+            <a href="<?= BASE?>cursos.php">Cursos</a>
+            <a href="<?= BASE?>duvidas.php">Dúvidas</a>
+            <a href="<?= BASE?>planos.php">Planos</a>
+            <a href="<?= BASE?>consultoria.php">Consultoria</a>
         </nav>
     </header>
 
