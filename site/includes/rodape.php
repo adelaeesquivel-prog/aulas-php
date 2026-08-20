36810f2d5943b7ce2b6018195b0f4853ff901ffd
adelaeesquivel-prog <adelaeesquivel@gmail.com>
2026-08-20 08:47:50 -0300

    css

--- site/includes/rodape.php
+++ site/includes/rodape.php
@@ -1,7 +1,8 @@
  </main>
 
-    <footer>
+    <footer class="text-center">
         <p>site criado usando PHP</p>
     </footer>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </body>
 </html>
